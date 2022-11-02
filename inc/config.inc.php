e32fa0dcbcc6238ab4be072e00940b57704b3f14
gontar <a.k.gontar02@mail.ru>
2022-11-02 17:26:13 +0800

    new

--- inc/config.inc.php
+++ inc/config.inc.php
@@ -8,9 +8,7 @@ const DB_NAME = "db_site";
 $link = mysqli_connect(DB_HOST, DB_LOGIN, DB_PASSWORD, DB_NAME);
 if ($link == false){
     print("Ошибка: Невозможно подключиться к MySQL " . mysqli_connect_error());
-    http_response_code(404);
-    include('..\\site_php\\404.php');
-    die();
+    err();
 }
 
 function sql($str){
@@ -20,17 +18,13 @@ function sql($str){
         $stmt = $link ->stmt_init();
         if($stmt->prepare($str) === false or $stmt->bind_param('i', $i) === false
         or $stmt->execute() === false or ($result = mysqli_stmt_get_result($stmt)) === false){
-            http_response_code(404);
-            include('..\\site_php\\404.php');
-            die();
+            err();
         }
         else{
             return $result;
         }  
     }else{
-        http_response_code(404);
-        include('..\\site_php\\404.php');
-        die();
+        err();
     }  
 }
 function price($string)
